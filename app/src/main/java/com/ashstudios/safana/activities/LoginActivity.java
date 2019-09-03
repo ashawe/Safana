63f4321267bf76f738ba7ef77cc2285403ac6779
Rohit Suthar <iamrohitsuthar@gmail.com>
2019-09-04 01:04:17 +0530

    1. hide status bar in login activity. 2. add emp id and password validation. 3. set focus to the password edit text after click on the emp id edit text right icon.

--- app/src/main/java/com/ashstudios/safana/activities/LoginActivity.java
+++ app/src/main/java/com/ashstudios/safana/activities/LoginActivity.java
@@ -11,9 +11,6 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
@@ -32,23 +29,30 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
         initialize();
         editText.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
                 if(motionEvent.getAction() == MotionEvent.ACTION_UP) {
-                    if((motionEvent.getRawX() >= (editText.getRight() - editText.getCompoundDrawables()[2].getBounds().width() - 20)) && editText.getText().length() != 10) {
-                        isSwipe = true;
-                        final AnimatorSet as = new AnimatorSet();
-                        ObjectAnimator animator1 = ObjectAnimator.ofFloat(findViewById(R.id.et_emp_id), "translationX", - constraintLayout.getWidth());
-                        ObjectAnimator animator2 = ObjectAnimator.ofFloat(findViewById(R.id.et_pwd), "translationX", 0);
-                        animator1.setDuration(500);
-                        animator2.setDuration(500);
-                        as.play(animator1);
-                        as.play(animator2);
-                        as.start();
-                        return true;
+                    if((motionEvent.getRawX() >= (editText.getRight() - editText.getCompoundDrawables()[2].getBounds().width() - 20))) {
+                        if(editText.getText().length() <= 0) {
+                            editText.setError("Enter proper id...");
+                        }
+                        else {
+                            isSwipe = true;
+                            final AnimatorSet as = new AnimatorSet();
+                            ObjectAnimator animator1 = ObjectAnimator.ofFloat(findViewById(R.id.et_emp_id), "translationX", - constraintLayout.getWidth());
+                            ObjectAnimator animator2 = ObjectAnimator.ofFloat(findViewById(R.id.et_pwd), "translationX", 0);
+                            animator1.setDuration(500);
+                            animator2.setDuration(500);
+                            as.play(animator1);
+                            as.play(animator2);
+                            as.start();
+                            editText1.requestFocus();
+                            return true;
+                        }
                     }
                 }
                 return false;
@@ -60,18 +64,20 @@ public class LoginActivity extends AppCompatActivity {
             public boolean onTouch(View view, MotionEvent motionEvent) {
                 if(motionEvent.getAction() == MotionEvent.ACTION_UP) {
                     if((motionEvent.getRawX() >= (editText1.getRight() - editText1.getCompoundDrawables()[2].getBounds().width() - 20)) && editText1.getText().length() != 10) {
-                        if( editText.getText().toString().equals("Emp123") && editText1.getText().toString().equals("emp123") )
-                        {
-                            intent = new Intent(getBaseContext(),WorkerDashboardActivity.class);
-                            startActivity(intent);
+                        if(editText1.getText().length() <= 0) {
+                            editText1.setError("Please enter password...");
                         }
-                        else if( editText.getText().toString().equals("Sup123") && editText1.getText().toString().equals("sup123") ) {
-                            intent = new Intent(getBaseContext(), SupervisorDashboard.class);
-                            startActivity(intent);
+                        else {
+                            if (editText.getText().toString().equals("Emp123") && editText1.getText().toString().equals("emp123")) {
+                                intent = new Intent(getBaseContext(), WorkerDashboardActivity.class);
+                                startActivity(intent);
+                            } else if (editText.getText().toString().equals("Sup123") && editText1.getText().toString().equals("sup123")) {
+                                intent = new Intent(getBaseContext(), SupervisorDashboard.class);
+                                startActivity(intent);
+                            } else
+                                Toast.makeText(context, "Wrong ID or password", Toast.LENGTH_SHORT).show();
+                            return true;
                         }
-                        else
-                            Toast.makeText(context, "Wrong ID or password", Toast.LENGTH_SHORT).show();
-                        return true;
                     }
                 }
                 return false;
