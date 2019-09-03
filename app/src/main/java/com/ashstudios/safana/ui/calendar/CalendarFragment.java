b3c75e441653bee6a95a1b52e3c2cab061930760
Rohit Suthar <iamrohitsuthar@gmail.com>
2019-09-03 17:26:54 +0530

    1. Implement calendar activity. 2. Display different date indicators in calendar like work, no work, and holidays. 3. add recycler view below the calendar to display the task that the worker has to complete on that selected date in calendar.

--- app/src/main/java/com/ashstudios/safana/ui/calendar/CalendarFragment.java
+++ app/src/main/java/com/ashstudios/safana/ui/calendar/CalendarFragment.java
@@ -1,35 +1,90 @@
 package com.ashstudios.safana.ui.calendar;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.applikeysolutions.cosmocalendar.settings.lists.connected_days.ConnectedDays;
+import com.applikeysolutions.cosmocalendar.view.CalendarView;
 import com.ashstudios.safana.R;
+import com.ashstudios.safana.adapters.TaskAdapter;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class CalendarFragment extends Fragment {
 
-    private CalendarViewModel galleryViewModel;
+    private CalendarViewModel calendarViewModel;
+    private RecyclerView recyclerView;
+    private CalendarView calendarView;
+    private TaskAdapter taskAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        galleryViewModel =
+        calendarViewModel =
                 ViewModelProviders.of(this).get(CalendarViewModel.class);
         View root = inflater.inflate(R.layout.fragment_calendar, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        galleryViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        //calendar view
+        calendarView = root.findViewById(R.id.calendar_view);
+        calendarView.setWeekendDays(new HashSet(){{add(Calendar.SUNDAY); add(Calendar.SATURDAY);}});
+        calendarView.setWeekendDayTextColor(Color.GREEN);
+        calendarView.setSelectedDayBackgroundColor(getResources().getColor(R.color.colorAccent));
+        //set not working days
+        setNoWorkDays("10/09/2019");
+        setNoWorkDays("12/09/2019");
+        //set working days
+        setWorkDays("07/09/2019");
+        setWorkDays("09/09/2019");
+        setWorkDays("11/09/2019");
+
+        recyclerView =  root.findViewById(R.id.rv_calendar_task);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        //set the adapter
+        taskAdapter = new TaskAdapter(getActivity(),calendarViewModel.getArrayListMutableLiveData());
+        recyclerView.setAdapter(taskAdapter);
         return root;
+    }
+
+    private void setNoWorkDays(String day) {
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat f = new SimpleDateFormat("dd/MM/yyyy");
+        Set<Long> days = new TreeSet<>();
+        try {
+            days.add(f.parse(day).getTime());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        int textColor = Color.parseColor("#ff0000");
+        int selectedTextColor = Color.parseColor("#ff4000");
+        ConnectedDays connectedDays = new ConnectedDays(days, textColor, selectedTextColor);
+        calendarView.addConnectedDays(connectedDays);
+    }
+
+    private void setWorkDays(String day) {
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat f = new SimpleDateFormat("dd/MM/yyyy");
+        Set<Long> days = new TreeSet<>();
+        try {
+            days.add(f.parse(day).getTime());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        int textColor = Color.parseColor("#0000FF");
+        ConnectedDays connectedDays = new ConnectedDays(days, textColor);
+        calendarView.addConnectedDays(connectedDays);
     }
 }
--- app/src/main/java/com/ashstudios/safana/ui/calendar/CalendarViewModel.java
+++ app/src/main/java/com/ashstudios/safana/ui/calendar/CalendarViewModel.java
@@ -4,16 +4,27 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.ashstudios.safana.models.TaskModel;
+
+import java.util.ArrayList;
+
 public class CalendarViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
-
+    private ArrayList<TaskModel> arrayListMutableLiveData;
     public CalendarViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is calendar fragment");
+        arrayListMutableLiveData = new ArrayList<>();
+        getData();
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public ArrayList<TaskModel> getArrayListMutableLiveData() {
+        return arrayListMutableLiveData;
+    }
+
+    public void getData() {
+        arrayListMutableLiveData.add(new TaskModel("Implement User Profile","28/03/2018"));
+        arrayListMutableLiveData.add(new TaskModel("Implement User Orders","28/04/2019"));
+        arrayListMutableLiveData.add(new TaskModel("Test User Profile","28/05/2019"));
+        arrayListMutableLiveData.add(new TaskModel("Feedback from Client","25/03/2019"));
+        arrayListMutableLiveData.add(new TaskModel("Implement User Profile","22/03/2019"));
     }
 }
