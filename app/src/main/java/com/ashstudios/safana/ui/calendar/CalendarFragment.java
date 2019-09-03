6d90593b2d5aa8c191bdb686139a32f165b6618a
Rohit Suthar <iamrohitsuthar@gmail.com>
2019-09-03 17:57:38 +0530

    1. hide the toolbar sorting icon. 2. move the working days and not working days method call in the onActivityCreated() call.

--- app/src/main/java/com/ashstudios/safana/ui/calendar/CalendarFragment.java
+++ app/src/main/java/com/ashstudios/safana/ui/calendar/CalendarFragment.java
@@ -2,11 +2,16 @@ package com.ashstudios.safana.ui.calendar;
 
 import android.annotation.SuppressLint;
 import android.graphics.Color;
+import android.opengl.Visibility;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.DefaultItemAnimator;
@@ -27,64 +32,74 @@ import java.util.TreeSet;
 
 public class CalendarFragment extends Fragment {
 
-    private CalendarViewModel calendarViewModel;
-    private RecyclerView recyclerView;
     private CalendarView calendarView;
-    private TaskAdapter taskAdapter;
+    @SuppressLint("SimpleDateFormat")
+    private SimpleDateFormat f = new SimpleDateFormat("dd/MM/yyyy");
+    private Set<Long> NoWorkdays, Workdays;
+    private int noWorkdaysColor = Color.parseColor("#ff0000");
+    private int workdaysColor = Color.parseColor("#0000ff");
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        calendarViewModel =
-                ViewModelProviders.of(this).get(CalendarViewModel.class);
+        CalendarViewModel calendarViewModel = ViewModelProviders.of(this).get(CalendarViewModel.class);
         View root = inflater.inflate(R.layout.fragment_calendar, container, false);
-
         //calendar view
         calendarView = root.findViewById(R.id.calendar_view);
         calendarView.setWeekendDays(new HashSet(){{add(Calendar.SUNDAY); add(Calendar.SATURDAY);}});
         calendarView.setWeekendDayTextColor(Color.GREEN);
         calendarView.setSelectedDayBackgroundColor(getResources().getColor(R.color.colorAccent));
-        //set not working days
-        setNoWorkDays("10/09/2019");
-        setNoWorkDays("12/09/2019");
-        //set working days
-        setWorkDays("07/09/2019");
-        setWorkDays("09/09/2019");
-        setWorkDays("11/09/2019");
+        NoWorkdays = new TreeSet<>();
+        Workdays = new TreeSet<>();
 
-        recyclerView =  root.findViewById(R.id.rv_calendar_task);
+        RecyclerView recyclerView = root.findViewById(R.id.rv_calendar_task);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         recyclerView.setHasFixedSize(true);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         //set the adapter
-        taskAdapter = new TaskAdapter(getActivity(),calendarViewModel.getArrayListMutableLiveData());
+        TaskAdapter taskAdapter = new TaskAdapter(getActivity(), calendarViewModel.getArrayListMutableLiveData());
         recyclerView.setAdapter(taskAdapter);
         return root;
     }
 
     private void setNoWorkDays(String day) {
-        @SuppressLint("SimpleDateFormat") SimpleDateFormat f = new SimpleDateFormat("dd/MM/yyyy");
-        Set<Long> days = new TreeSet<>();
         try {
-            days.add(f.parse(day).getTime());
+            NoWorkdays.add(f.parse(day).getTime());
         } catch (ParseException e) {
             e.printStackTrace();
         }
-        int textColor = Color.parseColor("#ff0000");
-        int selectedTextColor = Color.parseColor("#ff4000");
-        ConnectedDays connectedDays = new ConnectedDays(days, textColor, selectedTextColor);
+        ConnectedDays connectedDays = new ConnectedDays(NoWorkdays, noWorkdaysColor);
         calendarView.addConnectedDays(connectedDays);
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        //not working days
+        setNoWorkDays("10/09/2019");
+        //set working days
+        setWorkDays("07/09/2019");
+        setWorkDays("11/09/2019");
+    }
+
     private void setWorkDays(String day) {
-        @SuppressLint("SimpleDateFormat") SimpleDateFormat f = new SimpleDateFormat("dd/MM/yyyy");
-        Set<Long> days = new TreeSet<>();
         try {
-            days.add(f.parse(day).getTime());
+            Workdays.add(f.parse(day).getTime());
         } catch (ParseException e) {
             e.printStackTrace();
         }
-        int textColor = Color.parseColor("#0000FF");
-        ConnectedDays connectedDays = new ConnectedDays(days, textColor);
+        ConnectedDays connectedDays = new ConnectedDays(Workdays, workdaysColor);
         calendarView.addConnectedDays(connectedDays);
+    }
+
+    @Override
+    public void onPrepareOptionsMenu(Menu menu) {
+        menu.findItem(R.id.action_settings).setVisible(false);
+        super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
     }
 }
