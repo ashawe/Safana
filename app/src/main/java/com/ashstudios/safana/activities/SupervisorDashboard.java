6b0852cd9b1c168ee0f8081e3f10ce496c28294a
Ashawe <harshsaglani@gmail.com>
2019-09-04 15:49:54 +0530

    Click on nav header shows user profile. Set toolbar on task details. Set Custom theme on activities.

--- app/src/main/java/com/ashstudios/safana/activities/SupervisorDashboard.java
+++ app/src/main/java/com/ashstudios/safana/activities/SupervisorDashboard.java
@@ -1,8 +1,10 @@
 package com.ashstudios.safana.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -53,6 +55,13 @@ public class SupervisorDashboard extends AppCompatActivity {
         leaveSortBundle = new Bundle();
         taskSortBundle = new Bundle();
         menuItem = navigationView.getCheckedItem();
+        navigationView.getHeaderView(0).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(),WorkerProfileActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/ashstudios/safana/activities/TaskDetailsActivity.java
+++ app/src/main/java/com/ashstudios/safana/activities/TaskDetailsActivity.java
@@ -1,8 +1,10 @@
 package com.ashstudios.safana.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.ashstudios.safana.R;
 
@@ -12,5 +14,22 @@ public class TaskDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_details);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Safana");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
